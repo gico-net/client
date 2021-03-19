b70b5e9cf0d7c1ef66e7ef98f2a1f5edd8565891
Santo Cariotti <santo@dcariotti.me>
2021-03-19 14:23:46 +0100

    feat: reset vue projet to start

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="sass">
+@import 'node_modules/bootstrap/scss/bootstrap.scss'
+@import 'node_modules/bootstrap-vue/src/index.scss'
+@import "./sass/main.sass"
+</style>
