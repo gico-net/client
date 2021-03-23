fa76107eeca937667e10648e9bccb4decd343268
Santo Cariotti <santo@dcariotti.me>
2021-03-23 21:21:02 +0100

    feat: add search field

--- src/components/HeaderBlue.vue
+++ src/components/HeaderBlue.vue
@@ -3,6 +3,11 @@
     b-container
       h1
         a(href="/" rel="home") Gico
+      b-form(action="/search")
+        b-form-group
+          b-input(placeholder="Search a commit" name="q")
+        b-form-group
+          b-button(variant="primary" type="submit") Go
 </template>
 
 <script>
